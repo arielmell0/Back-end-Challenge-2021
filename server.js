e0f2a6bab2084d4f1999c27d3c2d1fb0024702cb
arielmell0 <arielmello@rede.ulbra.br>
2022-03-08 15:13:03 -0300

    feat: conectando banco de dados
    conectando nosso banco de dados Mongo e instalando o dotenv

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3030
 
 const homeRoute = require('./routes/home')
 const articleRoute = require('./routes/article')
+ 
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Banco de dados conectado')
+        app.emit('BancoOn')
+    })
+    .catch(error => console.log('Ops, ocorreu um erro. Código', error))
 
 app.use(express.json())
 
 app.use(homeRoute, articleRoute)
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
+app.on('BancoOn', () => {
+    app.listen(port, () => {
+        console.log(`Servidor rodando em http://localhost:${port}`)
+    })
 })
