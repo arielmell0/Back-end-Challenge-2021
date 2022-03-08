3bb2275c389d4643bdeb0b27d099e1105e84441a
arielmell0 <arielmello@rede.ulbra.br>
2022-03-08 00:00:32 -0300

    feat: controller da pagina inicial

---
+++ controllers/homeController.js
@@ -0,0 +1,3 @@
+exports.home = (req, res) => {
+    res.status(200).json({ message: 'Back-end Challenge 2021 🏅 - Space Flight News' })
+}
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 
 const homeController = require('../controllers/homeController')
 
-router.get('/', homeController)
+router.get('/', homeController.home)
 
 module.exports = router
