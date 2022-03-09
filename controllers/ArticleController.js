bf36d5944f5d5158acb87ffe90b9e791826e74ca
arielmell0 <arielmello@rede.ulbra.br>
2022-03-09 17:12:47 -0300

    feat: controler getArticleByID
    buscando os artigos do nosso banco de dados pelo id

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -9,3 +9,17 @@ exports.getArticles = async(req, res) => {
         res.status(500).json({ message: 'Ops, algum erro ocorreu.'})
     }
 }
+
+exports.getArticleByID = async(req, res) => {
+    const articleID = req.params.id
+
+    try {
+        const article = await ArticleModel.findById(articleID)
+
+        res.status(200).json({article})
+    } catch(error) {
+        res.status(500).json({ message: 'Ops! Algum erro ocorreu.' })
+
+        console.log('Um erro ocorreu, código: ', error)
+    }
+}
