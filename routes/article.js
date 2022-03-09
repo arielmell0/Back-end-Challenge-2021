1e70c0e15d8d465d2388a28e23dcf4717e32e523
arielmell0 <arielmello@rede.ulbra.br>
2022-03-09 18:49:56 -0300

    feat: rota post create article
    rota responsavel por criar um artigo no bd

--- routes/article.js
+++ routes/article.js
@@ -5,4 +5,6 @@ const ArticleController = require('../controllers/ArticleController')
 router.get('/articles', ArticleController.getArticles)
 router.get('/articles/:id', ArticleController.getArticleByID)
 
+router.post('/articles', ArticleController.createArticle)
+
 module.exports = router
