ae14054f86f9bc05998c91e2bb7683c97b783079
arielmell0 <arielmello@rede.ulbra.br>
2022-03-09 17:25:02 -0300

    feat: controller e route getArticlesById
    rota e controladora para buscar nossos artigos pelo id

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -14,11 +14,17 @@ exports.getArticleByID = async(req, res) => {
     const articleID = req.params.id
 
     try {
-        const article = await ArticleModel.findById(articleID)
+        const article = await ArticleModel.findOne({ _id: articleID })
+
+        if(!article) {
+            res.status(404).json({ message: 'Artigo não encontrado' })
+
+            return
+        }
 
         res.status(200).json({article})
     } catch(error) {
-        res.status(500).json({ message: 'Ops! Algum erro ocorreu.' })
+        res.status(500).json({ message: 'Ops! Algum erro ocorreu no servidor.' })
 
         console.log('Um erro ocorreu, código: ', error)
     }
--- routes/article.js
+++ routes/article.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 const ArticleController = require('../controllers/ArticleController')
 
 router.get('/articles', ArticleController.getArticles)
+router.get('/articles/:id', ArticleController.getArticleByID)
 
 module.exports = router
